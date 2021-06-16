8d8042d6241d1f1e46e662b4596a94ba66b686d5
Joshua Kenney <joshuagkenney@gmail.com>
2021-06-16 18:27:13 -0400

    working on belieflab server

---
+++ run.php
@@ -0,0 +1,28 @@
+<?php
+$post_data = json_decode(file_get_contents('php://input'), true); 
+// the directory "data" must be writable by the server
+$filename = "data/dsc_".$post_data['file'].".csv"; 
+$results = $post_data['results'];
+$score = $post_data['score'];
+$outcomes = $post_data['outcomes'];
+echo$filename;
+echo$score;
+echo$outcomes;
+echo$results;
+
+
+
+// write the file to disk
+// choose only one! otherwise the data will not save
+
+file_put_contents($filename, $results);
+// file_put_contents($filename, $score);
+// file_put_contents($filename, $outcomes);
+
+// $name = "data/".$post_data['filename'].".csv"; 
+// $results = $post_data['results'];
+// $score = $post_data['score'];
+// $outcomes = $post_data['outcomes'];
+// // write the file to disk
+// file_put_contents($name, $results, $score, $outcomes);
+
